d195502cc655f3237d931fef5812e457b67ec87b
Alex Jordan <alex@strugee.net>
2017-02-19 18:10:30 -0500

    Make the project use a build system
    This is step 1.

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    http = require('http'),
+    ecstatic = require('ecstatic'),
+    path = require('path'),
+    gutil = require('gulp-util'),
+    ghpages = require('gh-pages');
+
+gulp.task('build:html', function() {
+	gulp.src('src/*.html')
+	    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build:css', function() {
+	gulp.src('src/styles/*')
+	    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build:js', function() {
+	gulp.src('src/scripts/*')
+	    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', ['build'], function() {
+	gulp.watch('src/*.html', ['build:html']);
+	gulp.watch('src/styles/*', ['build:css']);
+	gulp.watch('src/scripts/*', ['build:js']);
+});
+
+gulp.task('serve', ['watch'], function() {
+        http.createServer(
+                ecstatic({ root: __dirname + '/dist' })
+        ).listen(8080);
+});
+
+gulp.task('deploy', ['build'], function(done) {
+        ghpages.publish(path.join(__dirname, 'dist'), { logger: gutil.log, branch: 'master' }, done);
+});
+
+gulp.task('build', ['build:html', 'build:css', 'build:js']);
+
+gulp.task('default', ['serve']);
